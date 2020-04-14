3357b9c2b5f5fc6340c98a8bce2928d97d810db6
flabby1976 <flabby1976@gmail.com>
2020-04-14 15:20:27 -0400

    Updates to VFDtest.py and serialVFD.py on the Pi

--- VFD/VFDtest.py
+++ VFD/VFDtest.py
@@ -9,7 +9,7 @@ time.sleep(5)
 print("now")
 
 # Initialise the lcd class
-lcd = serialVFD( ser, 20, 2)
+lcd = serialVFD.Serial_VFD( ser, 20, 2)
 
 # Print a two line message
 lcd.message = "Hello\nCircuitPython"
@@ -25,8 +25,10 @@ time.sleep(5)
 lcd.text_direction = lcd.LEFT_TO_RIGHT
 # Display cursor
 lcd.clear()
+print(lcd.cursor)
 lcd.cursor = True
 lcd.message = "Cursor! "
+print(lcd.cursor)
 # Wait 5s
 time.sleep(5)
 # Display blinking cursor
