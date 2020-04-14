5db66c2105b432e3889a5bc72c101f4666dbdf38
arobins <andrew_robinson_1977@yahoo.co.uk>
2020-04-13 21:27:07 -0400

    Further tidying of the Arduino code

--- FrontPanel/FrontPanel.ino
+++ FrontPanel/FrontPanel.ino
@@ -2,56 +2,94 @@
 #include <FR_RotaryEncoder.h>
 #include <Pushbutton.h>
 
-/*  Arduino code for iPud front panel buttons and switches    
+/*  Arduino code for iPud front panel display, buttons, dials and switches    
  *  by Andrew Robinson
  *  
  *  Uses Pushbutton library from https://github.com/pololu/pushbutton-arduino
  *  Uses rotary encoder library from https://github.com/fryktoria/FR_RotaryEncoder
  *          which is based on reading/debouncing from http://www.technoblogy.com/show?1YHJ
+ *  Uses SPI_VFD library from https://github.com/adafruit/SPI_VFD
  *  
  */
 
-
+// The 4 pushbuttons
 #define push1Pin 8
 #define push2Pin 9
 #define push3Pin 10
 #define push4Pin 11
 
+// The slider from the volume control pot
 #define volumePin A0
 
-int pinSCK = 3; // Interrupt pin for rotary encoder. Can be 2 or 3      
-int pinDT = 4; // Better select a pin from 4 to 7
-int pinSW = 2; // Interrupt pin for switch. Can be 2 or 3
-int rotaryMaximum = 100;
-int rotaryMinimum = -(rotaryMaximum - 1);
+// Rotary encoder pins
+#define pinSCK 3 // Interrupt pin for rotary encoder. Can be 2 or 3      
+#define pinDT 4  // Better select a pin from 4 to 7
+#define pinSW 2  // Interrupt pin for switch. Can be 2 or 3
+
+// SDI output for the display
+#define mSIO 5
+#define mSTB 6
+#define mSCK 7
+
+unsigned int rotaryMaximum = 1000;
+//int rotaryMinimum = -(rotaryMaximum - 1);
 bool rotaryWrapMode = false;
-
-int mSIO = 5;
-int mSTB = 6;
-int mSCK = 7;
-
-unsigned long button_time = 0;
 
 const byte numChars = 64;
 char receivedChars[numChars];
-
 boolean newData = false;
+
 
 //-----------------------------------------------------------------------------------
 
-// Push buttons
+// initialize the Push buttons
 Pushbutton push1(push1Pin);
 Pushbutton push2(push2Pin);
 Pushbutton push3(push3Pin);
 Pushbutton push4(push4Pin);
 
-// Rotary encoder
+// initialize the Rotary encoder
 RotaryEncoder rencoder(pinSCK, pinDT, pinSW);
+// Rotary encoder switch is handled better by the Pushbutton library
+Pushbutton pushk(pinSW);
 
-// Interrupt handling routine for the rotary
+// Interrupt handling routine for the rotary encoder
 void ISRrotary() {
     rencoder.rotaryUpdate();
 }
+
+// initialize the VFD with the numbers of the interface pins
+SPI_VFD vfd(mSIO, mSCK, mSTB);
+
+//-----------------------------------------------------------------------------------
+
+void setup() { 
+
+  // set up the LCD's number of columns and rows: 
+  vfd.begin(20, 2);
+  // Print a message to the LCD.
+
+  vfd.clear();
+  vfd.setCursor(0,0);
+  vfd.print(F("Arduino restarting ..."));
+
+  delay(1000);
+  
+  vfd.setCursor(0,0);  
+  vfd.print(F("hello, world!         "));
+
+  rencoder.enableInternalRotaryPullups(); 
+  rencoder.setRotaryLimits(-(rotaryMaximum - 1), rotaryMaximum, rotaryWrapMode);
+  
+  // One interrupt is required for the rotary
+  attachInterrupt(digitalPinToInterrupt(pinSCK), ISRrotary, CHANGE);
+  
+  // setup the serial port for comms with Pi
+  Serial.begin (9600);
+
+  Serial.println(">");
+} 
+
 
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
@@ -62,8 +100,6 @@ void recvWithStartEndMarkers() {
 
     while (Serial.available() > 0 && newData == false) {
         rc = Serial.read();
-
-//        Serial.write(rc);
 
         if (recvInProgress == true) {
             if (rc != endMarker) {
@@ -88,92 +124,99 @@ void recvWithStartEndMarkers() {
 }
 
 
-Pushbutton pushk(pinSW);
+void handleSwitch(Pushbutton *button, char *label) {
 
-// initialize the VFD with the numbers of the interface pins
-// (data, clk, strobe)
-SPI_VFD vfd(mSIO, mSCK, mSTB);
+   unsigned int t;
+   unsigned int button_time;
 
-int ledPin = 13;      // select the pin for the LED
-
-void setup() { 
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
-
-  // set up the LCD's number of columns and rows: 
-  vfd.begin(20, 2);
-  // Print a message to the LCD.
-  vfd.print("hello, world!");
-  
-  rencoder.enableInternalRotaryPullups(); 
-  rencoder.setRotaryLimits(rotaryMinimum, rotaryMaximum, rotaryWrapMode);
-  
-  // One interrupt is required for the rotary
-  attachInterrupt(digitalPinToInterrupt(pinSCK), ISRrotary, CHANGE);
-  
-  // setup the serial port for comms with Pi
-  Serial.begin (9600);
-
-  Serial.println("<Arduino is ready>");
-} 
-
-int t;
-int lastPosition;
-
-void loop() {
-
-   if (pushk.getSingleDebouncedPress()){
+   if ((*button).getSingleDebouncedPress()){
     button_time = millis();
     t = 0;
     // Note: this holds the whole loop up while button is pressed
-    while ( ( t < 1000) && pushk.isPressed() ) {
+    while ( ( t < 1000) && (*button).isPressed() ) {
       t = millis()-button_time;
       }
+    Serial.print("b ");
+    Serial.print(*label);
     if (t<1000)
-       Serial.println("knob button press");
+       Serial.println(" s");
     else
-       Serial.println("knob button longpress");    
+       Serial.println(" l");    
    }
+  
+}
 
- 
-   if (push1.getSingleDebouncedPress()){
-    button_time = millis();
-    t = 0;
-    while ( ( t < 1000) && push1.isPressed() ) {
-      t = millis()-button_time;
+void handleVFDcmd(char *cmd){
+
+  char *pch;
+  int a;
+  int b;
+  char *s;
+
+  pch = strtok(cmd," ");
+  if (pch != NULL) {
+    switch(pch[0]){
+      case 'h':{ // home and clear
+        vfd.clear();
+        break;
       }
-    if (t<1000)
-       Serial.println("button 1 press");
-    else
-       Serial.println("button 1 longpress");    
-   }
-   
-   if (push2.getSingleDebouncedPress()){
-    Serial.println("button 2 press");
-   }
-   if (push3.getSingleDebouncedPress()){
-    Serial.println("button 3 press");
-   }
-   if (push4.getSingleDebouncedPress()){
-    Serial.println("button 4 press");
-   }
+      case 'm': { // move cursor
+        a = atoi(strtok(NULL," "));
+        b = atoi(strtok(NULL," "));
+        vfd.setCursor(a, b);
+        break;
+      }
+      case 'p': { // print text
+        s = strtok(NULL,"\"");
+        vfd.print(s);
+        break;
+      }
+      case 's': { // scroll right or left
+        s = strtok(NULL," ");
+        if (s != NULL) {
+          if (s[0] == 'l') {
+            Serial.println(s[0]);
+            vfd.scrollDisplayLeft();
+          }
+          else {
+            vfd.scrollDisplayRight();
+          }
+         }
+        else {
+           vfd.scrollDisplayRight();
+        }        
+        break;
+      }
+      default:
+        Serial.println("?");
+        Serial.println(receivedChars);
+        break;
+    }
+  }
+}
+  
+
+int lastPosition;
+void loop() {
+
+  handleSwitch(&pushk, "k");
+  handleSwitch(&push1, "1");
+  handleSwitch(&push2, "2");
+  handleSwitch(&push3, "3");
+  handleSwitch(&push4, "4");
 
   // Rotary 
   int currentPosition = rencoder.getPosition();
   if (lastPosition != currentPosition) {
     lastPosition = currentPosition;
-    Serial.print("knob position ");Serial.println(currentPosition); 
+    Serial.print("k 1 ");Serial.println(currentPosition); 
   }
 
   recvWithStartEndMarkers();
   if (newData == true){
-    vfd.setCursor(0, 1);
-    vfd.print(receivedChars);
+    handleVFDcmd(receivedChars);
     newData = false;
   }
-  else {
-//    Serial.println("0");
-  }
  
- }
+}
  
